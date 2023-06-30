0994a420a7cffd1b5ed4e2e817efc345e1bb82d4
Morgan Evans <morgan.evans@selfridges.co.uk>
2023-06-30 14:18:40 +0100

    Initial commit

---
+++ test/personal.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Personal from '../lib/personal-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/personal-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Personal.PersonalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
