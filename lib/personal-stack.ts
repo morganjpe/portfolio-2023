5ea61e39690a9dea9644643bda837153de1bcb4b
Morgan Evans <morgan.evans@selfridges.co.uk>
2023-07-05 13:11:53 +0100

    github actions

--- lib/personal-stack.ts
+++ lib/personal-stack.ts
@@ -1,16 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { SPADeploy } from "cdk-spa-deploy";
 
 export class PersonalStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PersonalQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new SPADeploy(this, "client").createSiteWithCloudfront({
+      indexDoc: "index.html",
+      websiteFolder: `client/dist`,
+    });
   }
 }
