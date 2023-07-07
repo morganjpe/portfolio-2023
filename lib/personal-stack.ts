f0a694ef48dfb016a5bc042a16fb277e63fab92a
Morgan Evans <morgan.evans@selfridges.co.uk>
2023-07-07 15:49:19 +0100

    test actions

--- lib/personal-stack.ts
+++ lib/personal-stack.ts
@@ -8,7 +8,7 @@ export class PersonalStack extends cdk.Stack {
 
     new SPADeploy(this, "client").createSiteWithCloudfront({
       indexDoc: "index.html",
-      websiteFolder: "../client/dist",
+      websiteFolder: "client/dist",
     });
   }
 }
